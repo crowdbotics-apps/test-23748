6daf35176478e41a3499bb190d4203060bbc5bef
Crowdbotics <team@crowdbotics.com>
2021-01-10 04:11:46 +0000

    remove package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -8,7 +8,6 @@ export const installed_blueprints = [
 { name: 'Maps27191366', human_name: 'Maps 27', access_route: 'Maps27191366', icon: 'map'},
 { name: 'Messaging28191365', human_name: 'Messaging 28', access_route: 'Messaging28191365', icon: 'comments'},
 { name: 'Settings29191364', human_name: 'Settings 29', access_route: 'Settings29191364'},
-{ name: 'BlankScreen19191363', human_name: 'Blank Screen 19', access_route: 'BlankScreen19191363'},
 { name: 'ArticleList191328', human_name: 'Article List', access_route: 'ArticleList191328'},
 { name: 'ArticleList191327', human_name: 'Article List', access_route: 'ArticleList191327'},
 { name: 'ArticleList191326', human_name: 'Article List', access_route: 'ArticleList191326'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -13,7 +13,6 @@ import Feed25191368Navigator from '../features/Feed25191368/navigator';
 import Maps27191366Navigator from '../features/Maps27191366/navigator';
 import Messaging28191365Navigator from '../features/Messaging28191365/navigator';
 import Settings29191364Navigator from '../features/Settings29191364/navigator';
-import BlankScreen19191363Navigator from '../features/BlankScreen19191363/navigator';
 import ArticleList191328Navigator from '../features/ArticleList191328/navigator';
 import ArticleList191327Navigator from '../features/ArticleList191327/navigator';
 import ArticleList191326Navigator from '../features/ArticleList191326/navigator';
@@ -33,7 +32,6 @@ Feed25191368: { screen: Feed25191368Navigator },
 Maps27191366: { screen: Maps27191366Navigator },
 Messaging28191365: { screen: Messaging28191365Navigator },
 Settings29191364: { screen: Settings29191364Navigator },
-BlankScreen19191363: { screen: BlankScreen19191363Navigator },
 ArticleList191328: { screen: ArticleList191328Navigator },
 ArticleList191327: { screen: ArticleList191327Navigator },
 ArticleList191326: { screen: ArticleList191326Navigator },
